35d082428317d913fbaa4bc0b2169b1243a08a85
Vladislava Leonenko <aladka03@gmail.com>
2021-09-14 20:13:27 +0300

    Implemented classes Program and Trapeze

--- Execution/Program.cs
+++ Execution/Program.cs
@@ -1,12 +1,132 @@
-﻿using System;
+﻿using FigureLibrary;
+using System;
+using System.Xml;
 
 namespace Execution
 {
     class Program
     {
+        const string Filepath = @"..\myXmlFile.xml";
+        static XmlWriter xmlWriter;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                xmlWriter = XmlWriter.Create(Filepath);
+                Trapeze trapeze = Create();
+
+                int flag = 0;
+                while (flag != 7)
+                {
+                    Console.WriteLine("\n\t╔═══════════════════════════════════════════════════════════╗");
+                    Console.WriteLine("\t║                             МЕНЮ                          ║");
+                    Console.WriteLine("\t╟─────┬─────────────────────────────────────────────────────╢");
+                    Console.WriteLine("\t║  1  │              Создать новую трапецию                 ║");
+                    Console.WriteLine("\t╟─────┼─────────────────────────────────────────────────────╢");
+                    Console.WriteLine("\t║  2  │               Длины сторон трапеции                 ║");
+                    Console.WriteLine("\t╟─────┼─────────────────────────────────────────────────────╢");
+                    Console.WriteLine("\t║  3  │                Периметр трапеции                    ║");
+                    Console.WriteLine("\t╟─────┼─────────────────────────────────────────────────────╢");
+                    Console.WriteLine("\t║  4  │                 Площадь трапеции                    ║");
+                    Console.WriteLine("\t╟─────┼─────────────────────────────────────────────────────╢");
+                    Console.WriteLine("\t║  5  │           Проверить принадлежность точки            ║");
+                    Console.WriteLine("\t╟─────┼─────────────────────────────────────────────────────╢");
+                    Console.WriteLine("\t║  6  │            Сохранить данные в xml-файл              ║");
+                    Console.WriteLine("\t╟─────┼─────────────────────────────────────────────────────╢");
+                    Console.WriteLine("\t║  7  │                      Выход                          ║");
+                    Console.WriteLine("\t╚═══════════════════════════════════════════════════════════╝");
+
+                    flag = Convert.ToInt32(Console.ReadLine());
+                    switch (flag)
+                    {
+                        case 1:
+                            trapeze = Create();
+                            Console.WriteLine("\tНовая трапеция создана");
+                            break;
+                        case 2:
+                            Console.WriteLine($"\tСтороны трапеции: {trapeze.ViewSides()}");
+                            break;
+                        case 3:
+                            Console.WriteLine($"\tПериметр: {trapeze.Perimetr}");
+                            break;
+                        case 4:
+                            Console.WriteLine($"\tПлощадь: {trapeze.Square}");
+                            break;
+                        case 5:
+                            CheckPointAffiliation(trapeze);
+                            break;
+                        case 6:
+                            WriteToXml(trapeze);
+                            break;
+                        case 7:
+                            Console.WriteLine("\tПрограмма завершена");
+                            break;
+                        default:
+                            Console.WriteLine("\tТакого пункта меню нет");
+                            break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
+
+        /// <summary>
+        /// Trapeze creation
+        /// </summary>
+        /// <returns> Trapeze </returns>
+        static Trapeze Create()
+        {
+            Console.WriteLine("\tВведите две точки:");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double x2 = Convert.ToDouble(Console.ReadLine());
+
+            Trapeze trapeze = new Trapeze(x1, x2);
+
+            return trapeze;
+        }
+
+        /// <summary>
+        /// Checking point afflication to trapeze
+        /// </summary>
+        /// <param name="trapeze"> Selected trapeze </param>
+        static void CheckPointAffiliation(Trapeze trapeze)
+        {
+            Console.WriteLine("\tВведите координаты точки:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            if (trapeze.IsPointBelong(x, y))
+            {
+                Console.WriteLine("\tТочка принадлежит трапеции");
+            }
+            else
+            {
+                Console.WriteLine("\tТочка НЕ принадлежит трапеции");
+            }
+        }
+
+        /// <summary>
+        /// Writing trapeze info to xml-file
+        /// </summary>
+        /// <param name="trapeze"></param>
+        static void WriteToXml(Trapeze trapeze)
+        {
+            xmlWriter.WriteStartDocument();
+            xmlWriter.WriteStartElement("trapeze");
+
+            xmlWriter.WriteAttributeString("sides", trapeze.FormatSidesForXml());
+            xmlWriter.WriteAttributeString("perimetr", Convert.ToString(trapeze.Perimetr));
+            xmlWriter.WriteAttributeString("square", Convert.ToString(trapeze.Square));
+
+            xmlWriter.WriteEndDocument();
+            xmlWriter.Close();
+
+            Console.WriteLine("\tИнформация записана");
+        }
+
     }
 }
--- FigureLibrary/Trapeze.cs
+++ FigureLibrary/Trapeze.cs
@@ -4,5 +4,157 @@ namespace FigureLibrary
 {
     public class Trapeze
     {
+        const int IndexSide1 = 0,
+            IndexSide2 = 1,
+            IndexSide3 = 2,
+            IndexSide4 = 3;
+
+        private readonly double x1;
+        private readonly double x2;
+        private readonly double[] sides = new double[4];
+        public double Perimetr { get; }
+        public double Square { get; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="x1"> Point 1 on the Ox axis </param>
+        /// <param name="x2"> Point 2 on the Ox axis </param>
+        public Trapeze(double x1, double x2)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            CheckExistance();
+            FindSides();
+            Perimetr = FindPerimetr();
+            Square = FindSquare();
+        }
+
+        /// <summary>
+        /// Checking existanse of trapeze
+        /// </summary>
+        private void CheckExistance()
+        {
+            if (x1 * x2 < 0 || x1 == 0 || x2 == 0)
+            {
+                throw new Exception("Фигура не может существовать");
+            }
+        }
+
+        /// <summary>
+        /// Searching values of sides
+        /// </summary>
+        private void FindSides()
+        {
+            sides[IndexSide1] = Math.Abs(x2 - x1); // ось Ох
+            sides[IndexSide2] = Func(x1);
+            sides[IndexSide3] = Func(x2);
+            sides[IndexSide4] = CountIntegral();
+        }
+
+        /// <summary>
+        /// Searching perimetr
+        /// </summary>
+        /// <returns> Perimetr </returns>
+        private double FindPerimetr()
+        {
+            double p = 0;
+
+            foreach (double side in sides)
+            {
+                p += side;
+            }
+
+            return Math.Round(p, 3);
+        }
+
+        /// <summary>
+        /// Searching value of square
+        /// </summary>
+        /// <returns> Square </returns>
+        private double FindSquare()
+        {
+            return Math.Round(CountIntegral(), 3);
+        }
+
+        /// <summary>
+        /// Hyperbolic function
+        /// </summary>
+        /// <param name="x"> x parametr </param>
+        /// <returns> y parametr </returns>
+        private double Func(double x)
+        {
+            // функция гиперболы: y = 1 / x
+            return (1 / x);
+        }
+
+        /// <summary>
+        /// Counting value of integral
+        /// </summary>
+        /// <returns> Value of integral </returns>
+        private double CountIntegral()
+        {
+            int countOfSplits = 100;
+            double h = (x2 - x1) / countOfSplits;
+            double integral = 0;
+
+            for (int step = 0; step < countOfSplits; step++)
+            {
+                double a = x1 + step * h;
+                double b = x1 + (step + 1) * h;
+
+                integral += 0.5 * (b - a) * (Func(a) + Func(b));
+            }
+
+            return integral;
+        }
+
+        /// <summary>
+        /// Checking affiliation of point
+        /// </summary>
+        /// <param name="x"> x coordinate </param>
+        /// <param name="y"> y coordinate </param>
+        /// <returns> Is point belong (true or false) </returns>
+        public bool IsPointBelong(double x, double y)
+        {
+            if (((x >= x1 && x <= x2) || (x >= x2 && x <= x1)) && y <= Func(x))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Viewing values of sides
+        /// </summary>
+        /// <returns> Sides values </returns>
+        public string ViewSides()
+        {
+            string res = "";
+
+            foreach (double side in sides)
+            {
+                res += $"\n\t{Math.Round(side, 3)}; ";
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// Shaping string with values of sides to write them to xml-file
+        /// </summary>
+        /// <returns> String sides </returns>
+        public string FormatSidesForXml()
+        {
+            string res = "";
+
+            foreach (double side in sides)
+            {
+                res += $"{Math.Round(side, 3)}; ";
+            }
+
+            return res;
+        }
+
     }
 }
