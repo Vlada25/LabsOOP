11e5d8dda853cec4b7dcb43b172d95d5512b1bae
Vladislava Leonenko <aladka03@gmail.com>
2021-09-15 07:30:35 +0300

    Added more commens

--- FigureLibrary/IFigure.cs
+++ FigureLibrary/IFigure.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace FigureLibrary
 {
+    /// <summary>
+    /// Interface describing figures
+    /// </summary>
     public interface IFigure
     {
+        /// <summary>
+        /// Getting perimetr
+        /// </summary>
+        /// <returns> Perimetr </returns>
         double GetPerimetr();
+
+        /// <summary>
+        /// Getting square
+        /// </summary>
+        /// <returns> Square </returns>
         double GetSquare();
     }
 }
