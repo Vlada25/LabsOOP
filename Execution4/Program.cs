a96d765d366ab0473b0d8a3c6748df7c236d15a8
Vladislava Leonenko <aladka03@gmail.com>
2021-09-25 22:26:37 +0300

    Created methods Task_1 and Task_2

--- Execution4/Program.cs
+++ Execution4/Program.cs
@@ -1,9 +1,6 @@
 ﻿using NarayanaPermutationsLibrary;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Execution4
 {
@@ -11,9 +8,32 @@ namespace Execution4
     {
         static void Main()
         {
-            Console.WriteLine("Введите строку, разделяя слова знаком \"_\":");
-            string originalString = Console.ReadLine();
-            string[] words = originalString.Split('_');
+			Console.WriteLine("\nВыберите номер задания (1 или 2):");
+
+			int menuItem = int.Parse(Console.ReadLine());
+
+			switch (menuItem)
+            {
+				case 1:
+					Task_1();
+					break;
+				case 2:
+					Task_2();
+					break;
+				default:
+					Console.WriteLine("Такого пункта нет");
+					break;
+			}
+		}
+
+		/// <summary>
+		/// Task 1
+		/// </summary>
+		static void Task_1()
+        {
+			Console.WriteLine("\nВведите строку, разделяя слова знаком \"_\":");
+			string originalString = Console.ReadLine();
+			string[] words = originalString.Split('_');
 
 			Console.WriteLine("\nВыберите тип последовательности слов:\n" +
 				"1 - Неубывающая\n" +
@@ -21,8 +41,8 @@ namespace Execution4
 
 			int menuItem = int.Parse(Console.ReadLine());
 
-            switch (menuItem)
-            {
+			switch (menuItem)
+			{
 				case 1:
 					Array.Sort(words);
 
@@ -45,9 +65,46 @@ namespace Execution4
 				default:
 					Console.WriteLine("Такого пункта нет");
 					break;
-            }
+			}
 		}
 
+		/// <summary>
+		/// Task 2
+		/// </summary>
+		static void Task_2()
+        {
+			StringBuilder newText = new StringBuilder();
+
+			string givenText = "Обалдеть, с большой буквы «Х». То есть, когда доказательств не было," +
+				" я был сразу убийцей, а сейчас, когда виновный очевиден, «Миша просто ошибся». " +
+				"Нет, я не понимаю этого человека. С другой стороны, учитывая, что во время нашего прошлого " +
+				"разговора девушка подтвердила свое вольное или невольное участие в смертельном вандализме, " +
+				"может быть, это и не глупость. Надо бы понять, что именно она сделала. Тем более, я же под " +
+				"гейсом пообещал раскрыть тайну, надо держать слово.";
+
+			string[] allSentences = givenText.Split('.');
+
+			Console.WriteLine($"\nИсходный текст:\n{givenText}");
+
+			Console.WriteLine("\nВведите слово (для вывода строк, содержащих его)");
+			string word = Console.ReadLine();
+
+			foreach (string sentence in allSentences)
+            {
+				if (sentence.Contains(word))
+                {
+					newText.Append(sentence + ".");
+                }
+            }
+
+			Console.WriteLine($"\nПолученный текст:\n{newText}");
+		}
+
+		/// <summary>
+		/// Reversing array
+		/// </summary>
+		/// <param name="array">Selected array</param>
+		/// <returns>Rewersed array</returns>
 		static string[] Reverse(string[] array)
         {
 			string[] newArray = new string[array.Length];
