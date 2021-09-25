31452b58b3b30b8df8f54ec55bcd3b6f1ce8daa1
Vladislava Leonenko <aladka03@gmail.com>
2021-09-25 22:42:50 +0300

    Created unit test project

---
+++ NarayanaPermutationTest/PermutationTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NarayanaPermutationsLibrary;
+using System;
+
+namespace NarayanaPermutationTest
+{
+    [TestClass]
+    public class PermutationTest
+    {
+        [TestMethod]
+        public void NextPermutationTest()
+        {
+            string[] words = { "sad", "happy", "exelent" };
+            int counter = 0;
+            int expected = 6;
+
+            Array.Sort(words);
+
+            do 
+            {
+                counter++;
+            } while (Permutation.NextPermutation(words, Permutation.IsLess));
+
+            Assert.AreEqual(counter, expected);
+        }
+
+        [TestMethod]
+        [DataRow("hey", "array")]
+        public void IsGreaterTest(string word1, string word2)
+        {
+            Assert.IsTrue(Permutation.IsGreater(word1, word2));
+        }
+
+        [TestMethod]
+        [DataRow("hey", "array")]
+        public void IsLessTest(string word1, string word2)
+        {
+            Assert.IsTrue(Permutation.IsLess(word2, word1));
+        }
+    }
+}
---
+++ NarayanaPermutationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NarayanaPermutationTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NarayanaPermutationTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b5863d48-b2ea-43a0-8602-07c990e24242")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
