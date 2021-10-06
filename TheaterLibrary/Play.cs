14020562c8a58ff5851acb10fb03c957d16f0db8
Vladislava Leonenko <aladka03@gmail.com>
2021-10-06 15:09:34 +0300

    Created windows forms project and TheatreLibrary

---
+++ TheaterLibrary/Play.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheaterLibrary
+{
+    public class Play
+    {
+        public string Name { get; }
+        public TheaterGenre Genre { get; }
+        public List<DateTime> PremiereDate = new List<DateTime>();
+    }
+}
---
+++ TheaterLibrary/TheaterGenre.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheaterLibrary
+{
+    public enum TheaterGenre
+    {
+
+    }
+}
