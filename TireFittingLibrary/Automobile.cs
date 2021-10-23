d404e8fae42bb29fa780994d3547c726b6a81ab4
aladka03 <aladka03@gmail.com>
2021-10-23 11:53:21 +0300

    Implemented class Automobile

---
+++ TireFittingLibrary/Automobile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TireFittingLibrary
+{
+    public class Automobile
+    {
+        public string Model { get; }
+        public string Number { get; }
+
+        public Automobile(string model, string number)
+        {
+            Model = model;
+            Number = SetNumber(number);
+        }
+
+        private string SetNumber(string number)
+        {
+            Exception error = new Exception("Incorrect automobile number");
+
+            if (number.Length != 4)
+            {
+                throw error;
+            }
+
+            Regex numRegex = new Regex(@"\d{4}");
+            Match numMatch = numRegex.Match(number);
+
+            if (!numMatch.Success)
+            {
+                throw error;
+            }
+
+            return number;
+        }
+    }
+}
--- TireFittingLibrary/Repair.cs
+++ TireFittingLibrary/Repair.cs
@@ -4,5 +4,6 @@ namespace TireFittingLibrary
 {
     public class Repair
     {
+
     }
 }
